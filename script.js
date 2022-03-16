3870a993d8e0e8ca89b9d8096bde78e0202edc9b
ausjosh <joshuameadows91@gmail.com>
2022-03-16 14:30:48 -0500

    cleaned up code

--- script.js
+++ script.js
@@ -2,29 +2,24 @@
 let container = document.getElementById("grid");
 
 function createDiv(){
-
 let cell = document.createElement("div");
-cell.innerHTML = "TEXT";
+cell.innerHTML = '';
 container.appendChild(cell);
 }
 
-// funtion should create pre determined number of divs in a row
-function makeRow(){
+// funtion should create pre determined number of divs in a grid
+function makeGrid(){
     for(i= 0; i < 16; i++){
         createDiv()
+            for(j= 0; j < 16; j++){
+            createDiv()
+    
+    }
+
     }
 }
 
 
-function makeCollum(){
-    for(i= 0; i < 16; i++){
-        createDiv()
-}
-}
 
-function fullGrid(){
-makeRow()
-makeCollum()
-}
 
-fullGrid()
+makeGrid()
